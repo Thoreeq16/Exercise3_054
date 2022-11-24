afa0e275aeec0fbef35040f5d38e261141b6b658
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-11-24 15:58:11 +0700

    add method node prev

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -11,9 +11,10 @@ namespace Exercise3
         /*creates Nodes for the circular nested list
          * It consist of the information part and links to
          * its succeeding and preceeding nodes
-         * in terms of next nodes.*/
+         * in terms of next and previous nodes.*/
         public int rollNumber;
         public string name;
-        public Node next;
+        public Node next;/*points to the suceeding node*/
+        public Node prev;/*points to the suceeding node*/
     }
 }
